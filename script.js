18e8d0eec3050b5ef7aca7d5e8bd5257dc260462
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-14 10:44:06 -0600

    Roll Dice event listener generates a random dice roll and displays the corresponding image

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 'use strict';
 
-//Selecting Elements
+//Selecting elements
+const diceEl = document.querySelector('.dice');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
-//Default State
+//Default state
+diceEl.classList.add('hidden');
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //generate random roll between 1-6
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //display image of rolled dice number
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //check if roll is a 1, switch player
+});
