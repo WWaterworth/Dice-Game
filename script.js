97f114d0692a3bd9a04a5999146ab09cf5fc0baa
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-15 06:30:57 -0600

    Dice roll adds to current score if the roll is not a 1

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ const score1El = document.getElementById('score--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 //Default state
 diceEl.classList.add('hidden');
 score0El.textContent = 0;
 score1El.textContent = 0;
+
+let currentScore = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -23,5 +27,11 @@ btnRoll.addEventListener('click', function () {
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
 
-  //check if roll is a 1, switch player
+  //check if roll is a 1
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    //switch player
+  }
 });
