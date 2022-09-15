1e969f6242a253dc298ed5bb4f0a3d111c09edae
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-15 15:24:28 -0600

    New Game button resets state to default to start a new game

--- script.js
+++ script.js
@@ -21,7 +21,19 @@ const switchPlayer = () => {
   player1El.classList.toggle('player--active');
 };
 
-const scores = [0, 0];
+const newGame = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+};
+
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -36,7 +48,6 @@ btnRoll.addEventListener('click', function () {
   if (playing) {
     //generate random roll between 1-6
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
 
     //display image of rolled dice number
     diceEl.classList.remove('hidden');
@@ -77,4 +88,8 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  newGame();
 });
