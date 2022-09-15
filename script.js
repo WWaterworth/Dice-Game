85bd6bc9cddbfa9f4c02ec9c897c2831e934987a
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-15 09:08:30 -0600

    Event listener holds player's current score and adds it to total, then switches players

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 'use strict';
 
 //Selecting elements
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const diceEl = document.querySelector('.dice');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
 
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 const scores = [0, 0];
 let currentScore = 0;
@@ -37,8 +47,19 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     //switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  //Add current score to score of active player
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //Check if player's score is >= 100
+  //Finish Game
+
+  //Switch to next player
+  switchPlayer();
 });
